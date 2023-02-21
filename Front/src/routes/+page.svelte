33ea44f95d6b24ee9b306cfca429482771d29e37
Yassin Siouda <yassin@siouda.com>
2023-02-21 18:54:53 +0100

    init commit

---
+++ Front/src/routes/+page.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+
+    let apiUrl = "http://178.116.94.163:38001"
+
+    let pastMessage: Array = [
+        {content: "Bonjour je suis un assistant conversation, je suis la pour vous aider et répondre a vos problèmes que puis-je faire pour vous ?", sender: "ai"},
+    ]
+    let userMsg = ""
+    
+
+    onMount(async () => {
+
+    })
+
+
+    function sendMsg(e) {
+        console.log(e.key)
+        if (e.key === "Enter") {
+            pastMessage = [...pastMessage, {content: userMsg, sender: "human"}]
+            userMsg = ""
+            console.log(pastMessage)
+
+            // request message take avery message on the pastMessage array and send it to the api AI: msg \n HUMAN: msg
+            let requestMessage = ""
+            pastMessage.forEach((msg) => {
+                if (msg.sender === "human") {
+                    requestMessage = requestMessage + "HUMAN: " + msg.content + "\n"
+                } else {
+                    requestMessage = requestMessage + "AI: " + msg.content + "\n"
+                }
+            })
+            console.log("requets message",requestMessage)
+            requestMessage = requestMessage + "AI: "
+            // fetch post route with data array [ userMsg, 30, 1, 50, 0.95 ]
+            fetch(apiUrl + "/bloomz/run/predict" , {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    "data": [requestMessage, 4000, 1, 50, 0.95]
+                })
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                pastMessage = [...pastMessage, {content: data.data[0], sender: "human"}]
+            })
+        }
+    }
+
+</script>
+<section class="flex lg:flex-row h-screen flex-col">
+    <div class="bg-slate-800 text-white lg:w-2/12">
+        <ul>
+            <li>New Chat</li>
+            <li>LOL</li>
+            <li><input bind:value={apiUrl} class="text-black w-full"></li>
+        </ul>
+    </div>
+    <div class="lg:w-10/12 bg-gray-400 h-screen">
+        <div class="flex flex-col h-5/6">
+            {#each pastMessage as msg}
+                <div class="bg-white rounded-lg p-2 m-2">
+                    {msg.content}
+                </div>
+            {/each}
+        <input type="text" placeholder="Write message here" on:keydown={(e) => {sendMsg(e) }} bind:value={userMsg}/>
+    </div>
+</section>
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # OpenAI-CopyCat
 A copy of the openai interface
+
+
+## Deploy bloom server on vastai
+
+yassinsiouda/stableai-starterpack:0.3.2
+
+-p 8000 -p 8080 -e INSTANCE_ID=test -e FUNCTION_DOMAIN=test -e API_TOKEN=test
+
+sh entrypoint.sh
